8f6cdd17a46558abba17cae3d1880595078d8bc9
shanyao <sqshanyao@163.com>
2018-07-06 11:19:57 +0800

    新增开始页面

--- deploytool/src/com/yao/tool/ui/StartUI.java
+++ deploytool/src/com/yao/tool/ui/StartUI.java
@@ -1,4 +1,21 @@
 package com.yao.tool.ui;
 
-public class StartUI {
+import javax.swing.*;
+
+public class StartUI extends JFrame {
+    private JButton deployBtn;
+    private JButton updateBtn;
+
+    public StartUI() {
+        this.deployBtn = new JButton();
+        this.updateBtn = new JButton();
+
+        setDefaultCloseOperation(3);
+        setTitle("项目升级工具");
+        setMinimumSize(new java.awt.Dimension(550, 230));
+        setResizable(false);
+        getContentPane().setLayout(null);
+
+    }
+
 }
